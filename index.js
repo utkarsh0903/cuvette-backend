a6dcb51bd2c240d549d3828d4bd03cd3f66ab956
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-11-30 22:05:39 +0530

    send html file using path

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
-
+const path =require("path");
 const app = express();
 
+var filePath = path.join(__dirname, "public");
+
+app.use(express.static(filePath));
+
 app.get('/', (req, res)=> {
-    res.send('Utkarsh Garg || Frontend Developer')
+    res.sendFile(__dirname, "public", "index.html")
 })
 
 app.listen(3000, ()=> {
+    console.log(filePath)
     console.log(`Server running on port 3000`)
 })
