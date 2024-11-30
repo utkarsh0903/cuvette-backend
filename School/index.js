5ab50ea7530e028da6f4eb3b6a4821e7bdcb86f8
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-12-01 03:04:26 +0530

    Read the static database

--- School/index.js
+++ School/index.js
@@ -2,7 +2,27 @@ const express = require("express");
 const app = express();
 
 const studentArray = require("./initialData");
-console.log(studentArray)
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+app.get("/api/student/:id", (req, res) => {
+    let id = req.params.id;
+    if(!isNaN(id)){
+        let student = studentArray.find((item)=>{
+            return (item.id === parseInt(id));
+        })
+        if(student === undefined){
+            res.status(400).send("Student not found");
+        }
+        res.send(student);
+    }else{
+        res.status(400).send("Invalid id")
+    }
+    
+    res.send(student);
+})
 
 app.listen(4000, ()=> {
     console.log('Server running on port 4000');
