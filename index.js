cb80b02480bbaff43e14ad2d1667b20f77ad2a6a
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-11-30 22:17:29 +0530

    Create middleware

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ var filePath = path.join(__dirname, "public");
 
 app.use(express.static(filePath));
 
-app.get('/', (req, res)=> {
+const customMiddleware = (req, res, next) =>{
+    console.log("In Middleware");
+    next();
+}
+
+app.get('/',  (req, res)=> {
     res.sendFile(__dirname, "public", "index.html")
 })
 
+app.get('/me',customMiddleware, (req, res) => {
+    res.send("Hello Me");
+})
+
 app.listen(3000, ()=> {
-    console.log(filePath)
     console.log(`Server running on port 3000`)
 })
