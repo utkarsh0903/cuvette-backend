2449e6421cd16bdb454ce54945dd0ce83a8e7b04
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-12-06 02:29:57 +0530

    adding data using create method

--- mongoose/server.js
+++ mongoose/server.js
@@ -2,29 +2,34 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/pgpjuly").then(()=>{
-    console.log("Connected to mongodb")
-})
+mongoose.connect("mongodb://localhost:27017/pgpjuly").then(() => {
+  console.log("Connected to mongodb");
+});
 
 const studentSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    email: String
-})
+  name: String,
+  age: Number,
+  email: String,
+});
 
 const students = new mongoose.model("Student", studentSchema);
 
-const addData = ()=> {
-    const student1 = new students({
-        name: "Utkarsh",
-        age: 25,
-        email: "utkarshgarg871@gmail.com"
-    })
-    student1.save();
-}
+const addData = () => {
+  // const student1 = new students({   //adding new object to instance object
+  //     name: "Utkarsh",
+  //     age: 25,
+  //     email: "utkarshgarg871@gmail.com"
+  // })
+  // student1.save();     //to save the data
+  students.create({            //one more way to create data. 
+    name: "Rahul",
+    age: 26,
+    email: "rahuldhingra@gmail.com"
+  })
+};
 
 addData();
 
-app.listen(3000, ()=> {
-    console.log("Listening on port 3000")
-})
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
