fae69e7d09439ed36bfdc13d4d6bd9a456862ca8
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-12-06 01:56:50 +0530

    creating schema + adding data to collection

--- mongoose/server.js
+++ mongoose/server.js
@@ -1,11 +1,29 @@
 const express = require("express");
-const mongoose = require("mongoose");
-
 const app = express();
 
-mongoose.connect("mongoose://localhost:27017").then(()=>{
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/pgpjuly").then(()=>{
     console.log("Connected to mongodb")
 })
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    email: String
+})
+
+const students = new mongoose.model("Student", studentSchema);
+
+const addData = ()=> {
+    const student1 = new students({
+        name: "Utkarsh",
+        age: 25,
+        email: "utkarshgarg871@gmail.com"
+    })
+    student1.save();
+}
+
+addData();
 
 app.listen(3000, ()=> {
     console.log("Listening on port 3000")
