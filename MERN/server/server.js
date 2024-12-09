62f8c72fbf501e3bb27214eb75323784d8a727da
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-12-09 19:20:37 +0530

    user email exist check

--- MERN/server/server.js
+++ MERN/server/server.js
@@ -9,6 +9,10 @@ app.use(express.json());
 
 app.post("/register", async (req, res) =>{
     const {username, email, password} = req.body;
+    const user = await User.findOne({email});
+    if(user){
+       return res.status(400).json("User already exist")
+    }
     const hashedPass = await bcrypt.hash(password, 10);
     const newUser = new User({
         username,
