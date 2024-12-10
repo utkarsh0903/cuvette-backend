786f7ea32057999c391bf13998bc825d0f3482d2
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-12-10 13:23:18 +0530

    login

--- MERN/server/server.js
+++ MERN/server/server.js
@@ -28,6 +28,15 @@ app.post("/register", async (req, res) =>{
 //for login post as set up in rules because data is taken from frontend.
 app.post("/login", async (req, res)=>{
     const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+        return res.status(400).json({message:"User not found, please signup"});
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return res.status(400).json({message:"Password do not match"});
+    }
+    return res.json({message: "Login successful"})
 })
 
 app.listen(3000, ()=>{
