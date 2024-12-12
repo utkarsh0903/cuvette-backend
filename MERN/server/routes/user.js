fbf86ad82ea0b14d071e9aed50cc81c90c541233
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2024-12-13 01:04:09 +0530

    login route cleanup from server

--- MERN/server/routes/user.js
+++ MERN/server/routes/user.js
@@ -19,4 +19,18 @@ router.post("/register", async (req, res) =>{
     res.json("Registration done successfully");
 })
 
+//for login post as set up in rules because data is taken from frontend.
+router.post("/login", async (req, res)=>{
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+        return res.status(400).json({message:"User not found, please signup"});
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return res.status(400).json({message:"Password do not match"});
+    }
+    return res.json({message: "Login successful"})
+})
+
 module.exports = router;
--- MERN/server/server.js
+++ MERN/server/server.js
@@ -15,20 +15,6 @@ const port = process.env.PORT || 6000;
 
 app.use("/auth", router)
 
-//for login post as set up in rules because data is taken from frontend.
-app.post("/login", async (req, res)=>{
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
-    if(!user){
-        return res.status(400).json({message:"User not found, please signup"});
-    }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if(!isMatch){
-        return res.status(400).json({message:"Password do not match"});
-    }
-    return res.json({message: "Login successful"})
-})
-
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
 })
